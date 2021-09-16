1bc0a97f77b62cd1ca20d63632a3ab0849065a17
Josep Llaneras <pepmanuel@gmail.com>
2021-09-17 00:26:10 +0200

    Implement collision detection

--- tetris.js
+++ tetris.js
@@ -60,13 +60,35 @@ class Tetrimino {
 }
 
 class Field {
-    constructor(context) {
+    constructor(context, width, heigth) {
         this.context = context;
+        this.matrix = new Array(heigth).fill().map(
+            () => new Array(width).fill(0)
+        );
     }
 
     draw() {
         this.context.fillStyle = '#000';
         this.context.fillRect(0,0, canvas.width, canvas.height);
+    }
+
+    collision(tetrimino) {
+        for (let y = 0; y < tetrimino.shape.length; y++) {
+            let row = tetrimino.shape[y];
+            for (let x = 0; x < row.length; x++) {
+                let val = row[x];
+                if (val !== 0) {
+                    let posX = tetrimino.pos.x + x;
+                    let posY = tetrimino.pos.y + y;
+                    if (posX < 0 || posX >= this.matrix[0].length ||
+                        posY < 0 || posY >= this.matrix.length) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
     }
 }
 
@@ -74,25 +96,39 @@ class Tetris {
     constructor(elementId) {
         this.canvas = document.getElementById(elementId);
         this.context = canvas.getContext('2d');
-        this.context.scale(20, 20);
-        this.field = new Field(this.context);
-        this.tetrimino = new Tetrimino(Z, {x: 5, y: 5}, this.context);
+        const scale = {width: 20, height: 20}
+        this.context.scale(scale.width, scale.height);
+        this.field = new Field(
+            this.context, canvas.width/scale.width, canvas.height/scale.height
+        );
+        this.tetrimino = new Tetrimino(Z, {x: 6, y: 0}, this.context);
         this.elapsedTime = 0;
         this.dropInterval = 1000
         this.dropTime = 0
         this.setUpKeyBindings()
     }
 
+    collision() {
+        return this.field.collision(this.tetrimino);
+    }
+
+    move(x, y) {
+        this.tetrimino.move(x, y);
+        if (this.collision()) {
+            this.tetrimino.move(-x, -y)
+        }
+    }
+
     setUpKeyBindings() {
         document.addEventListener("keydown", (event) => {
             if (event.key === "ArrowLeft") {
-                tetris.tetrimino.move(-1, 0);
+                this.move(-1, 0);
             } else if (event.key === "ArrowRight") {
-                tetris.tetrimino.move(1, 0);
+                this.move(1, 0);
             } else if (event.key === "ArrowDown") {
-                tetris.tetrimino.move(0, 1);
+                this.move(0, 1);
             } else if (event.key === "ArrowUp") {
-                tetris.tetrimino.rotate()
+                this.tetrimino.rotate()
             }
         });
     }
@@ -107,7 +143,7 @@ class Tetris {
         this.elapsedTime = elapsedTime;
         this.dropTime += deltaTime;
         if (this.dropTime > this.dropInterval) {
-            this.tetrimino.pos.y++;
+            this.move(0, 1);
             this.dropTime = 0;
         }
         this.draw();
